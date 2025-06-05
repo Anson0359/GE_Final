690385aac34e56d29c11b15bf3134a9b9879ffef
Anson0359 <st0120680@gmail.com>
2025-06-05 18:28:27 +0800

    加入SetingUI的開關

--- Scripts/MainManager.cs
+++ Scripts/MainManager.cs
@@ -1,33 +1,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainManager
+public class MainManager : MonoBehaviour
 {
     private static MainManager instance = new MainManager();
     public static MainManager Instance { get { return instance; } }
 
     public enum Map_Type
     {
+        Home,
         MAP_1,
         MAP_2, 
         MAP_3
     }
 
-    [SerializeField] private GameObject homePage = null;
+    //[SerializeField] private GameObject homePage = null;
     [SerializeField] private GameObject loadingPage = null;
+    [SerializeField] private GameObject setingUI = null;
     [SerializeField] private List<GameObject> mapPrefabList = new List<GameObject>();
 
     private Map_Type nowMap = Map_Type.MAP_1;
+
 
 
     /// <summary>
     /// 開關Loading頁面
     /// </summary>
     /// <param name="active">開 / 關</param>
-    public void LoadingPage(bool active)
+    public void SetLoadingPage(bool active)
     {
         loadingPage.SetActive(active);
     }
+
+    public void SetSetingUI(bool active)
+    {
+        setingUI.SetActive(active);
+    }
+
+    public bool IsOpenSetingUI()
+    {
+        return setingUI.activeInHierarchy;
+    }
+
+
 
     public void SwichMap(Map_Type type)
     {
