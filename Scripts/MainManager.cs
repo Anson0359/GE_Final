11cda65c870b65885f14620cba4ac8c6f8e4b2ff
Anson0359 <st0120680@gmail.com>
2025-06-05 18:16:41 +0800

    新增管理器

---
+++ Scripts/MainManager.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager
+{
+    private static MainManager instance = new MainManager();
+    public static MainManager Instance { get { return instance; } }
+
+    public enum Map_Type
+    {
+        MAP_1,
+        MAP_2, 
+        MAP_3
+    }
+
+    [SerializeField] private GameObject homePage = null;
+    [SerializeField] private GameObject loadingPage = null;
+    [SerializeField] private List<GameObject> mapPrefabList = new List<GameObject>();
+
+    private Map_Type nowMap = Map_Type.MAP_1;
+
+
+    /// <summary>
+    /// 開關Loading頁面
+    /// </summary>
+    /// <param name="active">開 / 關</param>
+    public void LoadingPage(bool active)
+    {
+        loadingPage.SetActive(active);
+    }
+
+    public void SwichMap(Map_Type type)
+    {
+        if(nowMap == type)
+        {
+            Debug.LogWarning($"Same map can't switch , maptype = {type}");
+            return;
+        }
+
+        switch(type)
+        {
+            case Map_Type.MAP_1:
+                break;
+            case Map_Type.MAP_2:
+                break;
+            case Map_Type.MAP_3:
+                break;
+        }
+    }
+    
+}
