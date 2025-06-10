1a2238ad31f082815185f654a02880a03166d470
Anson0359 <st0120680@gmail.com>
2025-06-10 22:15:10 +0800

    Upate 202506102207

--- Scripts/Village.cs
+++ Scripts/Village.cs
@@ -1,6 +1,16 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class Village : MonoBehaviour
 {
-    
+    [SerializeField] private TextAsset talkContent = null;
+
+    private void Init()
+    {
+        if (talkContent != null)
+        {
+            List player = JsonUtility.FromJson<List>(talkContent.text);
+
+        }
+    }
 }
